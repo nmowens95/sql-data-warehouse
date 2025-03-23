b55337f5afd62c90c18fa34e5c07069d06354da5
Nathan Owens <126295718+nmowens95@users.noreply.github.com>
2025-03-23 13:57:41 -0500

    Update ddl_bronze.sql
    fixed spacing

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -39,12 +39,12 @@ CREATE TABLE bronze.sales_details (
 	sls_ord_num VARCHAR(50),
 	sls_prd_key VARCHAR(50),
 	sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
+	sls_order_dt INT,
+	sls_ship_dt  INT,
+	sls_due_dt   INT,
+	sls_sales    INT,
+	sls_quantity INT,
+	sls_price    INT
 );
 
 DROP TABLE IF EXISTS bronze.erp_loc_a101;
